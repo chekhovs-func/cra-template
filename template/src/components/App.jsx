7d619436d9c4ca76408c8cb37edd91d605ec61ce
Robert Butler <robertbutler@protonmail.com>
2022-03-14 11:15:10 +0000

    feat: implement styled-component

--- template/src/components/App.jsx
+++ template/src/components/App.jsx
@@ -1,9 +1,37 @@
-import '../styles/app.scss';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+  margin: 0;
+  box-sizing: border-box;
+  padding: 0;
+}
+
+main {
+  justify-content: center;
+  align-items: center;
+
+  display: flex;
+
+  width: 100vw;
+  height: 100vh;
+
+  background-color: rgba(0, 0, 0, 100%);
+
+  h1 {
+    color: rgba(255, 255, 255, 100%);
+  }
+}
+
+`;
 
 export default function App() {
   return (
-    <main>
-      <h1>It's alive!</h1>
-    </main>
+    <>
+      <GlobalStyle />
+      <main>
+        <h1>It's alive!</h1>
+      </main>
+    </>
   );
 }
